8fb9ec281d9f9c791e07d634c36a84593bb2783d
Tyler Cook <tcook@ustudio.com>
2020-09-28 20:29:17 -0500

    Added a constants file for display and easy referencing throughout the app

---
+++ src/constants.js
@@ -0,0 +1,81 @@
+export const INDUSTRY_AGRICULTURE = 'agriculture';
+export const INDUSTRY_APPAREL = 'apparel';
+export const INDUSTRY_BANKING = 'banking';
+export const INDUSTRY_BIOTECHNOLOGY = 'biotechnology';
+export const INDUSTRY_CHEMICALS = 'chemicals';
+export const INDUSTRY_COMMUNICATIONS = 'communications';
+export const INDUSTRY_CONSULTING = 'consulting';
+export const INDUSTRY_EDUCATION = 'education';
+export const INDUSTRY_ELECTRONICS = 'electronics';
+export const INDUSTRY_ENERGY = 'energy';
+export const INDUSTRY_ENTERTAINMENT = 'entertainment';
+export const INDUSTRY_ENVIRONMENTAL = 'environmental';
+export const INDUSTRY_FINANCE = 'finance';
+export const INDUSTRY_FOODANDBEVERAGE = 'foodandbeverage';
+export const INDUSTRY_GOVERNMENT = 'government';
+export const INDUSTRY_HEALTHCARE = 'healthcare';
+export const INDUSTRY_INSURANCE = 'insurance';
+export const INDUSTRY_MANUFACTURING = 'manufacturing';
+export const INDUSTRY_MEDIA = 'media';
+export const INDUSTRY_RECREATION = 'recreation';
+export const INDUSTRY_RETAIL = 'retail';
+export const INDUSTRY_SHIPPING = 'shipping';
+export const INDUSTRY_TECHNOLOGY = 'technology';
+export const INDUSTRY_TELECOMMUNICATIONS = 'telecommunications';
+export const INDUSTRY_TRANSPORTATION = 'transportation';
+export const INDUSTRY_UTILITIES = 'utilities';
+export const INDUSTRY_OTHER = 'other';
+
+export const INDUSTRY_OPTIONS = {
+  [INDUSTRY_AGRICULTURE]: 'Agriculture',
+  [INDUSTRY_APPAREL]: 'Apparel',
+  [INDUSTRY_BANKING]: 'Banking',
+  [INDUSTRY_BIOTECHNOLOGY]: 'Biotechnology',
+  [INDUSTRY_CHEMICALS]: 'Chemicals',
+  [INDUSTRY_COMMUNICATIONS]: 'Communications',
+  [INDUSTRY_CONSULTING]: 'Consulting',
+  [INDUSTRY_EDUCATION]: 'Education',
+  [INDUSTRY_ELECTRONICS]: 'Electronics',
+  [INDUSTRY_ENERGY]: 'Energy',
+  [INDUSTRY_ENTERTAINMENT]: 'Entertainment',
+  [INDUSTRY_ENVIRONMENTAL]: 'Environmental',
+  [INDUSTRY_FINANCE]: 'Finance',
+  [INDUSTRY_FOODANDBEVERAGE]: 'Food & Beverage',
+  [INDUSTRY_GOVERNMENT]: 'Government',
+  [INDUSTRY_HEALTHCARE]: 'Healthcare',
+  [INDUSTRY_INSURANCE]: 'Insurance',
+  [INDUSTRY_MANUFACTURING]: 'Manufacturing',
+  [INDUSTRY_MEDIA]: 'Media',
+  [INDUSTRY_RECREATION]: 'Recreation',
+  [INDUSTRY_RETAIL]: 'Retail',
+  [INDUSTRY_SHIPPING]: 'Shipping',
+  [INDUSTRY_TECHNOLOGY]: 'Technology',
+  [INDUSTRY_TELECOMMUNICATIONS]: 'Telecommunications',
+  [INDUSTRY_TRANSPORTATION]: 'Transportation',
+  [INDUSTRY_UTILITIES]: 'Utilities',
+  [INDUSTRY_OTHER]: 'Other'
+};
+
+export const RAITING_HOT = 'hot';
+export const RAITING_WARM = 'warm';
+export const RAITING_COLD = 'cold';
+
+export const RAITING_OPTIONS = {
+  [RAITING_HOT]: 'Hot',
+  [RAITING_WARM]: 'Warm',
+  [RAITING_COLD]: 'Cold'
+};
+
+export const LEAD_SOURCE_WEB = 'web';
+export const LEAD_SOURCE_PHONE = 'phone';
+export const LEAD_SOURCE_PARTNERREFERRAL = 'partnerreferral';
+export const LEAD_SOURCE_PURCHASEDLIST = 'purchasedlist';
+export const LEAD_SOURCE_OTHER = 'other';
+
+export const LEAD_SOURCE_OPTIONS = {
+  [LEAD_SOURCE_WEB]: 'Web',
+  [LEAD_SOURCE_PHONE]: 'Phone',
+  [LEAD_SOURCE_PARTNERREFERRAL]: 'Partner Referral',
+  [LEAD_SOURCE_PURCHASEDLIST]: 'Purchased List',
+  [LEAD_SOURCE_OTHER]: 'Other'
+};
