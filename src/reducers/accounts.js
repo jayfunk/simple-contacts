ba92287297bbc2e3826ab323b929c6229e7515c1
Tyler Cook <tcook@ustudio.com>
2020-09-28 21:10:51 -0500

    Added all of the crud operations to the reducer

--- src/reducers/accounts.js
+++ src/reducers/accounts.js
@@ -1,5 +1,107 @@
+import {
+  createAccount,
+  updateAccount,
+  removeAccount,
+  addContactToAccount,
+  updateContact,
+  removeContactFromAccount
+} from '../actions/accounts';
+
 export default function Accounts(state = getInitialState(), action = {}) {
   switch (action.type) {
+    case createAccount.type: {
+      const account = action.payload;
+      account.contacts = [];
+
+      return state.concat(account);
+    }
+
+    case updateAccount.type: {
+      const accountIndex = state.findIndex((account) => account.id === action.payload.accountId);
+      const account = state[accountIndex];
+
+      const updatedAccount = {
+        ...account,
+        ...action.payload.update,
+        id: account.id,
+        contacts: account.contacts
+      };
+
+      const updatedState = state.slice();
+      updatedState[accountIndex] = updatedAccount;
+
+      return updatedState;
+    }
+
+    case removeAccount.type: {
+      const accountIndex = state.findIndex((account) => account.id === action.payload);
+      const updatedState = state.slice();
+
+      updatedState.splice(accountIndex, 1);
+
+      return updatedState;
+    }
+
+    case addContactToAccount.type: {
+      const accountIndex = state.findIndex((account) => account.id === action.payload.accountId);
+      const account = state[accountIndex];
+
+      const updatedAccount = {
+        ...account,
+        contacts: [action.payload.contact, ...account.contacts]
+      };
+
+      const updatedState = state.slice();
+      updatedState[accountIndex] = updatedAccount;
+
+      return updatedState;
+    }
+
+    case updateContact.type: {
+      const accountIndex = state.findIndex((account) => account.id === action.payload.accountId);
+      const account = state[accountIndex];
+
+      const contacts = account.contacts.slice();
+      const contactIndex = contacts.findIndex((contact) => contact.id === action.payload.contactId);
+      const contact = contacts[contactIndex];
+
+      contacts[contactIndex] = {
+        ...contact,
+        ...action.payload.update,
+        id: contact.id
+      };
+
+      const updatedAccount = {
+        ...account,
+        contacts
+      };
+
+      const updatedState = state.slice();
+      updatedState[accountIndex] = updatedAccount;
+
+      return updatedState;
+    }
+
+    case removeContactFromAccount.type: {
+      const accountIndex = state.findIndex((account) => account.id === action.payload.accountId);
+      const account = state[accountIndex];
+
+      const contacts = account.contacts.slice();
+      const contactIndex = contacts.findIndex((contact) => contact.id === action.payload.contactId);
+
+      contacts.splice(contactIndex, 1);
+
+      const updatedAccount = {
+        ...account,
+        contacts
+      };
+
+      const updatedState = state.slice();
+      updatedState[accountIndex] = updatedAccount;
+
+      return updatedState;
+    }
+
     default:
       return state;
   }
--- src/reducers/accounts.test.js
+++ src/reducers/accounts.test.js
@@ -1,15 +1,347 @@
+import * as constants from '../constants';
 import reducer from './accounts';
+import {
+  createAccount,
+  updateAccount,
+  removeAccount,
+  addContactToAccount,
+  updateContact,
+  removeContactFromAccount
+} from '../actions/accounts';
 
-test('accounts sets its default state', () => {
+it('should set its default state to an empty array', () => {
   expect(reducer()).toEqual([]);
 });
 
-test('accounts adds a new account', () => {});
+it('should create a new account', () => {
+  expect(
+    reducer(
+      reducer(),
+      createAccount({
+        name: 'New Account',
+        address: '1234 mini lane boston, ma',
+        industry: constants.INDUSTRY_APPAREL
+      })
+    )
+  ).toEqual([
+    {
+      id: expect.anything(),
+      name: 'New Account',
+      address: '1234 mini lane boston, ma',
+      industry: constants.INDUSTRY_APPAREL,
+      contacts: []
+    }
+  ]);
+});
 
-test('accounts updates an existing account', () => {});
+it('should update an existing account', () => {
+  const account1Action = createAccount({
+    name: 'Account 1',
+    address: '1234 mini lane boston, ma',
+    industry: constants.INDUSTRY_APPAREL
+  });
+  const account2Action = createAccount({
+    name: 'Account 2',
+    address: '1 congress ave austin, tx',
+    raiting: constants.RAITING_HOT
+  });
+  const accountId = account1Action.payload.id;
+  let state = reducer(reducer(), account1Action);
+  state = reducer(state, account2Action);
 
-test('accounts deletes an account', () => {});
+  expect(
+    reducer(
+      state,
+      updateAccount(accountId, {
+        rating: constants.RAITING_HOT,
+        name: 'Updated Account Name'
+      })
+    )
+  ).toEqual([
+    {
+      id: accountId,
+      name: 'Updated Account Name',
+      address: '1234 mini lane boston, ma',
+      industry: constants.INDUSTRY_APPAREL,
+      rating: constants.RAITING_HOT,
+      contacts: []
+    },
+    {
+      id: expect.anything(),
+      name: 'Account 2',
+      address: '1 congress ave austin, tx',
+      raiting: constants.RAITING_HOT,
+      contacts: []
+    }
+  ]);
+});
 
-test('accounts adds a contact to an account', () => {});
+it('should update an account and ignore an id in the update payload ', () => {
+  const account1Action = createAccount({
+    name: 'Account 1',
+    address: '1234 mini lane boston, ma',
+    industry: constants.INDUSTRY_APPAREL
+  });
+  const account2Action = createAccount({
+    name: 'Account 2',
+    address: '1 congress ave austin, tx',
+    raiting: constants.RAITING_HOT
+  });
+  const accountId = account1Action.payload.id;
+  let state = reducer(reducer(), account1Action);
+  state = reducer(state, account2Action);
 
-test('accounts removes a contact from an account', () => {});
+  expect(
+    reducer(
+      state,
+      updateAccount(accountId, {
+        id: 1234,
+        rating: constants.RAITING_HOT,
+        name: 'Updated Account Name'
+      })
+    )
+  ).toEqual([
+    {
+      id: accountId,
+      name: 'Updated Account Name',
+      address: '1234 mini lane boston, ma',
+      industry: constants.INDUSTRY_APPAREL,
+      rating: constants.RAITING_HOT,
+      contacts: []
+    },
+    {
+      id: expect.anything(),
+      name: 'Account 2',
+      address: '1 congress ave austin, tx',
+      raiting: constants.RAITING_HOT,
+      contacts: []
+    }
+  ]);
+});
+
+it('should update an account and ignore a contacts field in the update payload ', () => {
+  const account1Action = createAccount({
+    name: 'Account 1',
+    address: '1234 mini lane boston, ma',
+    industry: constants.INDUSTRY_APPAREL
+  });
+  const accountId = account1Action.payload.id;
+  const state = reducer(reducer(), account1Action);
+
+  expect(
+    reducer(
+      state,
+      updateAccount(accountId, {
+        rating: constants.RAITING_HOT,
+        name: 'Updated Account Name',
+        contacts: ['something']
+      })
+    )
+  ).toEqual([
+    {
+      id: accountId,
+      name: 'Updated Account Name',
+      address: '1234 mini lane boston, ma',
+      industry: constants.INDUSTRY_APPAREL,
+      rating: constants.RAITING_HOT,
+      contacts: []
+    }
+  ]);
+});
+
+it('should delete an account', () => {
+  const account1Action = createAccount({
+    name: 'Account 1',
+    address: '1234 mini lane boston, ma',
+    industry: constants.INDUSTRY_APPAREL
+  });
+  const account2Action = createAccount({
+    name: 'Account 2',
+    address: '1 congress ave austin, tx',
+    raiting: constants.RAITING_HOT
+  });
+  const accountId = account2Action.payload.id;
+  let state = reducer(reducer(), account1Action);
+  state = reducer(state, account2Action);
+
+  expect(reducer(state, removeAccount(accountId))).toEqual([
+    {
+      id: expect.anything(),
+      name: 'Account 1',
+      address: '1234 mini lane boston, ma',
+      industry: constants.INDUSTRY_APPAREL,
+      contacts: []
+    }
+  ]);
+});
+
+it('should add a contact to an account', () => {
+  const account1Action = createAccount({
+    name: 'Account 1',
+    address: '1234 mini lane boston, ma',
+    industry: constants.INDUSTRY_APPAREL
+  });
+  const accountId = account1Action.payload.id;
+  const state = reducer(reducer(), account1Action);
+
+  const addContactAction = addContactToAccount(accountId, {
+    name: 'Contact 1',
+    phone: '5555555555',
+    email: 'contact1@contacts.com',
+    leadSource: constants.LEAD_SOURCE_PURCHASEDLIST
+  });
+
+  expect(reducer(state, addContactAction)).toEqual([
+    {
+      id: accountId,
+      name: 'Account 1',
+      address: '1234 mini lane boston, ma',
+      industry: constants.INDUSTRY_APPAREL,
+      contacts: [
+        {
+          id: addContactAction.payload.contact.id,
+          name: 'Contact 1',
+          phone: '5555555555',
+          email: 'contact1@contacts.com',
+          leadSource: constants.LEAD_SOURCE_PURCHASEDLIST
+        }
+      ]
+    }
+  ]);
+});
+
+it('should update an accounts contact', () => {
+  const account1Action = createAccount({
+    name: 'Account 1',
+    address: '1234 mini lane boston, ma',
+    industry: constants.INDUSTRY_APPAREL
+  });
+  const accountId = account1Action.payload.id;
+
+  const addContactAction = addContactToAccount(accountId, {
+    name: 'Contact 1',
+    phone: '5555555555',
+    email: 'contact1@contacts.com',
+    leadSource: constants.LEAD_SOURCE_PURCHASEDLIST
+  });
+  const contactId = addContactAction.payload.contact.id;
+
+  let state = reducer(reducer(), account1Action);
+  state = reducer(state, addContactAction);
+
+  const updateContactAction = updateContact(accountId, contactId, {
+    name: 'Updated Contact 1',
+    phone: '1234567890'
+  });
+
+  expect(reducer(state, updateContactAction)).toEqual([
+    {
+      id: accountId,
+      name: 'Account 1',
+      address: '1234 mini lane boston, ma',
+      industry: constants.INDUSTRY_APPAREL,
+      contacts: [
+        {
+          id: contactId,
+          name: 'Updated Contact 1',
+          phone: '1234567890',
+          email: 'contact1@contacts.com',
+          leadSource: constants.LEAD_SOURCE_PURCHASEDLIST
+        }
+      ]
+    }
+  ]);
+});
+
+it('should update an accounts contact and ignore an id field in the update', () => {
+  const account1Action = createAccount({
+    name: 'Account 1',
+    address: '1234 mini lane boston, ma',
+    industry: constants.INDUSTRY_APPAREL
+  });
+  const accountId = account1Action.payload.id;
+
+  const addContactAction = addContactToAccount(accountId, {
+    name: 'Contact 1',
+    phone: '5555555555',
+    email: 'contact1@contacts.com',
+    leadSource: constants.LEAD_SOURCE_PURCHASEDLIST
+  });
+  const contactId = addContactAction.payload.contact.id;
+
+  let state = reducer(reducer(), account1Action);
+  state = reducer(state, addContactAction);
+
+  const updateContactAction = updateContact(accountId, contactId, {
+    name: 'Updated Contact 1',
+    phone: '1234567890',
+    id: 1234
+  });
+
+  expect(reducer(state, updateContactAction)).toEqual([
+    {
+      id: accountId,
+      name: 'Account 1',
+      address: '1234 mini lane boston, ma',
+      industry: constants.INDUSTRY_APPAREL,
+      contacts: [
+        {
+          id: contactId,
+          name: 'Updated Contact 1',
+          phone: '1234567890',
+          email: 'contact1@contacts.com',
+          leadSource: constants.LEAD_SOURCE_PURCHASEDLIST
+        }
+      ]
+    }
+  ]);
+});
+
+it('should remove a contact from an account', () => {
+  const account1Action = createAccount({
+    name: 'Account 1',
+    address: '1234 mini lane boston, ma',
+    industry: constants.INDUSTRY_APPAREL
+  });
+  const accountId = account1Action.payload.id;
+
+  const addContact1Action = addContactToAccount(accountId, {
+    name: 'Contact 1',
+    phone: '5555555555',
+    email: 'contact1@contacts.com',
+    leadSource: constants.LEAD_SOURCE_PURCHASEDLIST
+  });
+  const addContact2Action = addContactToAccount(accountId, {
+    name: 'Contact 2',
+    phone: '3333333333',
+    email: 'contact2@contacts.com',
+    leadSource: constants.LEAD_SOURCE_PHONE
+  });
+  const contactId = addContact1Action.payload.contact.id;
+  const contact2Id = addContact2Action.payload.contact.id;
+
+  let state = reducer(reducer(), account1Action);
+  state = reducer(state, addContact1Action);
+  state = reducer(state, addContact2Action);
+
+  const removeContactAction = removeContactFromAccount(accountId, contactId);
+
+  const actual = reducer(state, removeContactAction);
+  expect(actual).toEqual([
+    {
+      id: accountId,
+      name: 'Account 1',
+      address: '1234 mini lane boston, ma',
+      industry: constants.INDUSTRY_APPAREL,
+      contacts: [
+        {
+          id: contact2Id,
+          name: 'Contact 2',
+          phone: '3333333333',
+          email: 'contact2@contacts.com',
+          leadSource: constants.LEAD_SOURCE_PHONE
+        }
+      ]
+    }
+  ]);
+});
