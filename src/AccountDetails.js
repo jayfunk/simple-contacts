83cd6ab87eecacb1b3c27b0d483e37e3c0cf8f73
Tyler Cook <tcook@ustudio.com>
2020-09-29 01:12:56 -0500

    Laid out the account details page form

--- src/AccountDetails.js
+++ src/AccountDetails.js
@@ -1,10 +1,61 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {Form, Button, Col} from 'react-bootstrap';
+
+import {INDUSTRY_OPTIONS, RATING_OPTIONS} from './constants';
 
 class AccountDetails extends Component {
-  render() {}
+  render() {
+    return (
+      <div className="col">
+        <Form>
+          <Form.Row>
+            <Col>
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" />
+            </Col>
+            <Col>
+              <Form.Label>Physical Address</Form.Label>
+              <Form.Control type="text" />
+            </Col>
+            <Col>
+              <Form.Label>Industry</Form.Label>
+              <Form.Control as="select">
+                {Object.keys(INDUSTRY_OPTIONS).map((optKey) => (
+                  <option value={optKey}>{INDUSTRY_OPTIONS[optKey]}</option>
+                ))}
+              </Form.Control>
+            </Col>
+          </Form.Row>
+          <Form.Row>
+            <Col>
+              <Form.Label>Annual Revenue</Form.Label>
+              <Form.Control type="text" />
+            </Col>
+            <Col>
+              <Form.Label>Rating</Form.Label>
+              <Form.Control as="select">
+                {Object.keys(RATING_OPTIONS).map((optKey) => (
+                  <option value={optKey}>{RATING_OPTIONS[optKey]}</option>
+                ))}
+              </Form.Control>
+            </Col>
+            <Col>
+              <Form.Label>Establised Date</Form.Label>
+              <Form.Control type="date" />
+            </Col>
+          </Form.Row>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
 
-AccountDetails.propTypes = {};
+AccountDetails.propTypes = {
+  account: PropTypes.object
+};
 
 export default AccountDetails;
