29c060022a661106f43279a4e6808c12320f60ae
Tyler Cook <tcook@ustudio.com>
2020-09-30 12:00:27 -0500

    Added the ability to filter accounts by name, industry, revenue, rating and date

--- src/Accounts.js
+++ src/Accounts.js
@@ -1,58 +1,232 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {Link, withRouter} from 'react-router-dom';
-import {FormControl, InputGroup} from 'react-bootstrap';
+import {FormControl, Form} from 'react-bootstrap';
 
 import {RATING_OPTIONS, INDUSTRY_OPTIONS} from './constants';
 
 import './Accounts.css';
 
-function renderAccounts(history, accounts) {
-  return accounts.map((account) => {
-    return (
-      <tr
-        key={account.id}
-        onClick={() => {
-          history.push(`/accounts/${account.id}`);
-        }}
-      >
-        <td>{account.name}</td>
-        <td>{account.address}</td>
-        <td>{INDUSTRY_OPTIONS[account.industry]}</td>
-        <td>{account.annualRevenue}</td>
-        <td>{RATING_OPTIONS[account.rating]}</td>
-        <td>{account.establishedDate.toString()}</td>
-      </tr>
-    );
-  });
-}
+export class Accounts extends Component {
+  constructor(props) {
+    super(props);
 
-export function Accounts(props) {
-  return (
-    <div className="accounts">
-      <InputGroup className="mb-3">
-        <InputGroup.Prepend>
-          <Link className="btn btn-primary" to="/accounts/new">
-            Add Account
-          </Link>
-        </InputGroup.Prepend>
-        <FormControl placeholder="Filter" />
-      </InputGroup>
-      <table className="table">
-        <thead className="thead-dark">
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Address</th>
-            <th scope="col">Industry</th>
-            <th scope="col">Revenue</th>
-            <th scope="col">Rating</th>
-            <th scope="col">Est. Date</th>
+    this.state = {
+      name: '',
+      industry: '',
+      rating: '',
+      annualRevenue: {
+        greaterAnnualRevenue: '',
+        lessAnnualRevenue: ''
+      },
+      establishedDate: {
+        afterEstablishedDate: '',
+        beforeEstablishedDate: ''
+      }
+    };
+
+    this.handleOneDimensionalFilter = this.handleOneDimensionalFilter.bind(this);
+    this.handleRevenueChange = this.handleRevenueChange.bind(this);
+    this.handleEstablishedDateChange = this.handleEstablishedDateChange.bind(this);
+    this.filterByName = this.filterByName.bind(this);
+    this.filterBySelectOption = this.filterBySelectOption.bind(this);
+    this.filterByGreaterAnnualRevenue = this.filterByGreaterAnnualRevenue.bind(this);
+    this.filterByLessAnnualRevenue = this.filterByLessAnnualRevenue.bind(this);
+    this.filterByAfterEstablishedDate = this.filterByAfterEstablishedDate.bind(this);
+    this.filterByBeforeEstablishedDate = this.filterByBeforeEstablishedDate.bind(this);
+  }
+
+  handleOneDimensionalFilter(event) {
+    const target = event.target;
+    const name = target.name;
+    this.setState({
+      ...this.state,
+      [name]: target.value
+    });
+  }
+
+  handleRevenueChange(event) {
+    const target = event.target;
+    const name = target.name;
+    this.setState({
+      annualRevenue: {
+        ...this.state.annualRevenue,
+        [name]: target.value
+      }
+    });
+  }
+
+  handleEstablishedDateChange(event) {
+    const target = event.target;
+    const name = target.name;
+    this.setState({
+      establishedDate: {
+        ...this.state.establishedDate,
+        [name]: target.value
+      }
+    });
+  }
+
+  filterByName(account) {
+    if (this.state.name) {
+      return account.name.includes(this.state.name);
+    }
+    return true;
+  }
+
+  filterBySelectOption(fieldKey, account) {
+    const filter = this.state[fieldKey];
+
+    if (!filter) {
+      return true;
+    }
+
+    return account[fieldKey] === filter;
+  }
+
+  filterByGreaterAnnualRevenue(account) {
+    const greaterAnnualRevenue = Number.parseFloat(this.state.annualRevenue.greaterAnnualRevenue);
+
+    if (Number.isNaN(greaterAnnualRevenue) === false) {
+      return account.annualRevenue >= greaterAnnualRevenue;
+    }
+    return true;
+  }
+
+  filterByLessAnnualRevenue(account) {
+    const lessAnnualRevenue = Number.parseFloat(this.state.annualRevenue.lessAnnualRevenue);
+
+    if (Number.isNaN(lessAnnualRevenue) === false) {
+      return account.annualRevenue <= lessAnnualRevenue;
+    }
+    return true;
+  }
+
+  convertDateStringToDate(dateString) {
+    const [year, month, date] = dateString.split('-');
+    return new Date(year, Number.parseInt(month) - 1, date);
+  }
+
+  filterByAfterEstablishedDate(account) {
+    if (this.state.establishedDate.afterEstablishedDate) {
+      return (
+        this.convertDateStringToDate(account.establishedDate).getTime() >=
+        this.convertDateStringToDate(this.state.establishedDate.afterEstablishedDate).getTime()
+      );
+    }
+    return true;
+  }
+
+  filterByBeforeEstablishedDate(account) {
+    if (this.state.establishedDate.beforeEstablishedDate) {
+      return (
+        this.convertDateStringToDate(account.establishedDate).getTime() <=
+        this.convertDateStringToDate(this.state.establishedDate.beforeEstablishedDate).getTime()
+      );
+    }
+    return true;
+  }
+
+  renderAccounts() {
+    return this.props.accounts
+      .filter(this.filterByName)
+      .filter(this.filterBySelectOption.bind(this, 'industry'))
+      .filter(this.filterBySelectOption.bind(this, 'rating'))
+      .filter(this.filterByGreaterAnnualRevenue)
+      .filter(this.filterByLessAnnualRevenue)
+      .filter(this.filterByAfterEstablishedDate)
+      .filter(this.filterByBeforeEstablishedDate)
+      .map((account) => {
+        return (
+          <tr
+            key={account.id}
+            onClick={() => {
+              this.props.history.push(`/accounts/${account.id}`);
+            }}
+          >
+            <td>{account.name}</td>
+            <td>{account.address}</td>
+            <td>{INDUSTRY_OPTIONS[account.industry]}</td>
+            <td>{account.annualRevenue}</td>
+            <td>{RATING_OPTIONS[account.rating]}</td>
+            <td>{account.establishedDate.toString()}</td>
           </tr>
-        </thead>
-        <tbody>{renderAccounts(props.history, props.accounts)}</tbody>
-      </table>
-    </div>
-  );
+        );
+      });
+  }
+
+  render() {
+    return (
+      <div className="accounts">
+        <Link className="btn btn-primary" to="/accounts/new">
+          Add Account
+        </Link>
+        <Form>
+          <Form.Row>
+            <FormControl
+              name="name"
+              type="text"
+              placeholder="Name"
+              onChange={this.handleOneDimensionalFilter}
+            />
+            <FormControl
+              placeholder="State"
+              name="state"
+              type="text"
+              onChange={this.handleFilterChange}
+            />
+            <FormControl name="industry" as="select" onChange={this.handleOneDimensionalFilter}>
+              {Object.keys(INDUSTRY_OPTIONS).map((optKey) => (
+                <option key={optKey} value={optKey}>
+                  {INDUSTRY_OPTIONS[optKey]}
+                </option>
+              ))}
+            </FormControl>
+            <FormControl
+              name="greaterAnnualRevenue"
+              type="number"
+              onChange={this.handleRevenueChange}
+            />
+            <FormControl
+              name="lessAnnualRevenue"
+              type="number"
+              onChange={this.handleRevenueChange}
+            />
+            <FormControl name="rating" as="select" onChange={this.handleOneDimensionalFilter}>
+              {Object.keys(RATING_OPTIONS).map((optKey) => (
+                <option key={optKey} value={optKey}>
+                  {RATING_OPTIONS[optKey]}
+                </option>
+              ))}
+            </FormControl>
+            <Form.Control
+              name="afterEstablishedDate"
+              type="date"
+              onChange={this.handleEstablishedDateChange}
+            />
+            <Form.Control
+              name="beforeEstablishedDate"
+              type="date"
+              onChange={this.handleEstablishedDateChange}
+            />
+          </Form.Row>
+        </Form>
+        <table className="table">
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Address</th>
+              <th scope="col">Industry</th>
+              <th scope="col">Revenue</th>
+              <th scope="col">Rating</th>
+              <th scope="col">Est. Date</th>
+            </tr>
+          </thead>
+          <tbody>{this.renderAccounts()}</tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 Accounts.propTypes = {
--- src/Accounts.test.js
+++ src/Accounts.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import {nanoid} from '@reduxjs/toolkit';
 import {shallow} from 'enzyme';
+import {FormControl} from 'react-bootstrap';
 
-import {RATING_HOT, INDUSTRY_AGRICULTURE} from './constants';
+import {RATING_HOT, INDUSTRY_AGRICULTURE, INDUSTRY_MEDIA, RATING_WARM} from './constants';
 import {Accounts} from './Accounts';
+
+function changeFormControlValue(wrapper, name, value) {
+  wrapper
+    .find(FormControl)
+    .find({name})
+    .simulate('change', {target: {value, name}});
+}
 
 it('should render an account', () => {
   const account = {
@@ -11,7 +19,7 @@ it('should render an account', () => {
     name: 'New Account',
     address: '102 my street austin, texas',
     industry: INDUSTRY_AGRICULTURE,
-    annualRevenue: '$1.2B',
+    annualRevenue: 1000,
     rating: RATING_HOT,
     establishedDate: '2019-12-31'
   };
@@ -22,7 +30,7 @@ it('should render an account', () => {
   expect(wrapper.find('tbody tr td').at(0)).toIncludeText('New Account');
   expect(wrapper.find('tbody tr td').at(1)).toIncludeText('102 my street austin, texas');
   expect(wrapper.find('tbody tr td').at(2)).toIncludeText('Agriculture');
-  expect(wrapper.find('tbody tr td').at(3)).toIncludeText('$1.2B');
+  expect(wrapper.find('tbody tr td').at(3)).toIncludeText(1000);
   expect(wrapper.find('tbody tr td').at(4)).toIncludeText('Hot');
   expect(wrapper.find('tbody tr td').at(5)).toIncludeText(account.establishedDate);
 });
@@ -41,7 +49,7 @@ it('should navigate to an account detail page when an account row is clicked', (
           name: 'New Account',
           address: '102 my street austin, texas',
           industry: INDUSTRY_AGRICULTURE,
-          annualRevenue: '$1.2B',
+          annualRevenue: 1000,
           rating: RATING_HOT,
           establishedDate: '2012-01-22'
         }
@@ -53,4 +61,440 @@ it('should navigate to an account detail page when an account row is clicked', (
   wrapper.find('tbody tr').simulate('click');
 
   expect(historyMock.push).toHaveBeenCalledWith(`/accounts/${accountId}`);
+});
+
+it('should filter by name', () => {
+  const wrapper = shallow(
+    <Accounts
+      accounts={[
+        {
+          id: nanoid(),
+          name: 'Account 1',
+          address: '100 my street austin, texas',
+          industry: INDUSTRY_MEDIA,
+          annualRevenue: 1000,
+          rating: RATING_WARM,
+          establishedDate: '2010-04-14'
+        },
+        {
+          id: nanoid(),
+          name: 'Account 2',
+          address: '102 my street austin, texas',
+          industry: INDUSTRY_AGRICULTURE,
+          annualRevenue: 100000,
+          rating: RATING_HOT,
+          establishedDate: '2012-01-22'
+        }
+      ]}
+      history={{}}
+    />
+  );
+
+  changeFormControlValue(wrapper, 'name', '2');
+
+  expect(wrapper.find('tbody').children().length).toEqual(1);
+  expect(
+    wrapper
+      .find('tbody tr td')
+      .first()
+      .text()
+  ).toEqual('Account 2');
+});
+
+it('should filter by address', () => {});
+
+it('should filter by industry', () => {
+  const wrapper = shallow(
+    <Accounts
+      accounts={[
+        {
+          id: nanoid(),
+          name: 'Account 1',
+          address: '100 my street austin, texas',
+          industry: INDUSTRY_MEDIA,
+          annualRevenue: 1000,
+          rating: RATING_WARM,
+          establishedDate: '2010-04-14'
+        },
+        {
+          id: nanoid(),
+          name: 'Account 2',
+          address: '102 my street austin, texas',
+          industry: INDUSTRY_AGRICULTURE,
+          annualRevenue: 100000,
+          rating: RATING_HOT,
+          establishedDate: '2012-01-22'
+        }
+      ]}
+      history={{}}
+    />
+  );
+
+  changeFormControlValue(wrapper, 'industry', INDUSTRY_MEDIA);
+
+  expect(wrapper.find('tbody').children().length).toEqual(1);
+  expect(
+    wrapper
+      .find('tbody tr td')
+      .first()
+      .text()
+  ).toEqual('Account 1');
+  expect(
+    wrapper
+      .find('tbody tr td')
+      .at(2)
+      .text()
+  ).toEqual('Media');
+});
+
+it('should filter by annualRevenue when greater than', () => {
+  const wrapper = shallow(
+    <Accounts
+      accounts={[
+        {
+          id: nanoid(),
+          name: 'Account 1',
+          address: '100 my street austin, texas',
+          industry: INDUSTRY_MEDIA,
+          annualRevenue: 1000,
+          rating: RATING_WARM,
+          establishedDate: '2010-04-14'
+        },
+        {
+          id: nanoid(),
+          name: 'Account 2',
+          address: '102 my street austin, texas',
+          industry: INDUSTRY_AGRICULTURE,
+          annualRevenue: 100000,
+          rating: RATING_HOT,
+          establishedDate: '2012-01-22'
+        }
+      ]}
+      history={{}}
+    />
+  );
+
+  changeFormControlValue(wrapper, 'greaterAnnualRevenue', 1001);
+
+  expect(wrapper.find('tbody').children().length).toEqual(1);
+  expect(
+    wrapper
+      .find('tbody tr td')
+      .first()
+      .text()
+  ).toEqual('Account 2');
+
+  expect(
+    wrapper
+      .find('tbody tr td')
+      .at(3)
+      .text()
+  ).toEqual('100000');
+});
+
+it('should filter by annualRevenue when less than', () => {
+  const wrapper = shallow(
+    <Accounts
+      accounts={[
+        {
+          id: nanoid(),
+          name: 'Account 1',
+          address: '100 my street austin, texas',
+          industry: INDUSTRY_MEDIA,
+          annualRevenue: 1000,
+          rating: RATING_WARM,
+          establishedDate: '2010-04-14'
+        },
+        {
+          id: nanoid(),
+          name: 'Account 2',
+          address: '102 my street austin, texas',
+          industry: INDUSTRY_AGRICULTURE,
+          annualRevenue: 100000,
+          rating: RATING_HOT,
+          establishedDate: '2012-01-22'
+        }
+      ]}
+      history={{}}
+    />
+  );
+
+  changeFormControlValue(wrapper, 'lessAnnualRevenue', 1001);
+
+  expect(wrapper.find('tbody').children().length).toEqual(1);
+  expect(
+    wrapper
+      .find('tbody tr td')
+      .first()
+      .text()
+  ).toEqual('Account 1');
+
+  expect(
+    wrapper
+      .find('tbody tr td')
+      .at(3)
+      .text()
+  ).toEqual('1000');
+});
+
+it('should filter by annualRevenue when greater than and less than', () => {
+  const wrapper = shallow(
+    <Accounts
+      accounts={[
+        {
+          id: nanoid(),
+          name: 'Account 1',
+          address: '100 my street austin, texas',
+          industry: INDUSTRY_MEDIA,
+          annualRevenue: 10,
+          rating: RATING_WARM,
+          establishedDate: '2010-04-14'
+        },
+        {
+          id: nanoid(),
+          name: 'Account 2',
+          address: '100 my street austin, texas',
+          industry: INDUSTRY_MEDIA,
+          annualRevenue: 1000,
+          rating: RATING_WARM,
+          establishedDate: '2010-04-14'
+        },
+        {
+          id: nanoid(),
+          name: 'Account 3',
+          address: '102 my street austin, texas',
+          industry: INDUSTRY_AGRICULTURE,
+          annualRevenue: 100000,
+          rating: RATING_HOT,
+          establishedDate: '2012-01-22'
+        }
+      ]}
+      history={{}}
+    />
+  );
+
+  changeFormControlValue(wrapper, 'greaterAnnualRevenue', 11);
+  changeFormControlValue(wrapper, 'lessAnnualRevenue', 2000);
+
+  expect(wrapper.find('tbody').children().length).toEqual(1);
+  expect(
+    wrapper
+      .find('tbody tr td')
+      .first()
+      .text()
+  ).toEqual('Account 2');
+
+  expect(
+    wrapper
+      .find('tbody tr td')
+      .at(3)
+      .text()
+  ).toEqual('1000');
+});
+
+it('should filter by rating', () => {
+  const wrapper = shallow(
+    <Accounts
+      accounts={[
+        {
+          id: nanoid(),
+          name: 'Account 1',
+          address: '100 my street austin, texas',
+          industry: INDUSTRY_MEDIA,
+          annualRevenue: 1000,
+          rating: RATING_WARM,
+          establishedDate: '2010-04-14'
+        },
+        {
+          id: nanoid(),
+          name: 'Account 2',
+          address: '102 my street austin, texas',
+          industry: INDUSTRY_AGRICULTURE,
+          annualRevenue: 100000,
+          rating: RATING_HOT,
+          establishedDate: '2012-01-22'
+        }
+      ]}
+      history={{}}
+    />
+  );
+
+  changeFormControlValue(wrapper, 'rating', RATING_HOT);
+
+  expect(wrapper.find('tbody').children().length).toEqual(1);
+  expect(
+    wrapper
+      .find('tbody tr td')
+      .first()
+      .text()
+  ).toEqual('Account 2');
+  expect(
+    wrapper
+      .find('tbody tr td')
+      .at(4)
+      .text()
+  ).toEqual('Hot');
+});
+
+it('should filter by establishedDate when after', () => {
+  const wrapper = shallow(
+    <Accounts
+      accounts={[
+        {
+          id: nanoid(),
+          name: 'Account 1',
+          address: '100 my street austin, texas',
+          industry: INDUSTRY_MEDIA,
+          annualRevenue: 1000,
+          rating: RATING_WARM,
+          establishedDate: '2019-04-14'
+        },
+        {
+          id: nanoid(),
+          name: 'Account 2',
+          address: '100 my street austin, texas',
+          industry: INDUSTRY_MEDIA,
+          annualRevenue: 1000,
+          rating: RATING_WARM,
+          establishedDate: '2019-05-14'
+        },
+        {
+          id: nanoid(),
+          name: 'Account 3',
+          address: '102 my street austin, texas',
+          industry: INDUSTRY_AGRICULTURE,
+          annualRevenue: 100000,
+          rating: RATING_HOT,
+          establishedDate: '2020-01-22'
+        }
+      ]}
+      history={{}}
+    />
+  );
+
+  changeFormControlValue(wrapper, 'afterEstablishedDate', '2019-05-15');
+
+  expect(wrapper.find('tbody').children().length).toEqual(1);
+  expect(
+    wrapper
+      .find('tbody tr td')
+      .first()
+      .text()
+  ).toEqual('Account 3');
+
+  expect(
+    wrapper
+      .find('tbody tr td')
+      .at(5)
+      .text()
+  ).toEqual('2020-01-22');
+});
+
+it('should filter by establishedDate when before', () => {
+  const wrapper = shallow(
+    <Accounts
+      accounts={[
+        {
+          id: nanoid(),
+          name: 'Account 1',
+          address: '100 my street austin, texas',
+          industry: INDUSTRY_MEDIA,
+          annualRevenue: 1000,
+          rating: RATING_WARM,
+          establishedDate: '2019-04-14'
+        },
+        {
+          id: nanoid(),
+          name: 'Account 2',
+          address: '100 my street austin, texas',
+          industry: INDUSTRY_MEDIA,
+          annualRevenue: 1000,
+          rating: RATING_WARM,
+          establishedDate: '2019-05-14'
+        },
+        {
+          id: nanoid(),
+          name: 'Account 3',
+          address: '102 my street austin, texas',
+          industry: INDUSTRY_AGRICULTURE,
+          annualRevenue: 100000,
+          rating: RATING_HOT,
+          establishedDate: '2020-01-22'
+        }
+      ]}
+      history={{}}
+    />
+  );
+
+  changeFormControlValue(wrapper, 'beforeEstablishedDate', '2019-05-13');
+
+  expect(wrapper.find('tbody').children().length).toEqual(1);
+  expect(
+    wrapper
+      .find('tbody tr td')
+      .first()
+      .text()
+  ).toEqual('Account 1');
+
+  expect(
+    wrapper
+      .find('tbody tr td')
+      .at(5)
+      .text()
+  ).toEqual('2019-04-14');
+});
+
+it('should filter by establishedDate when before and after', () => {
+  const wrapper = shallow(
+    <Accounts
+      accounts={[
+        {
+          id: nanoid(),
+          name: 'Account 1',
+          address: '100 my street austin, texas',
+          industry: INDUSTRY_MEDIA,
+          annualRevenue: 1000,
+          rating: RATING_WARM,
+          establishedDate: '2019-04-14'
+        },
+        {
+          id: nanoid(),
+          name: 'Account 2',
+          address: '100 my street austin, texas',
+          industry: INDUSTRY_MEDIA,
+          annualRevenue: 1000,
+          rating: RATING_WARM,
+          establishedDate: '2019-05-14'
+        },
+        {
+          id: nanoid(),
+          name: 'Account 3',
+          address: '102 my street austin, texas',
+          industry: INDUSTRY_AGRICULTURE,
+          annualRevenue: 100000,
+          rating: RATING_HOT,
+          establishedDate: '2020-01-22'
+        }
+      ]}
+      history={{}}
+    />
+  );
+
+  changeFormControlValue(wrapper, 'afterEstablishedDate', '2019-04-22');
+  changeFormControlValue(wrapper, 'beforeEstablishedDate', '2020-01-01');
+
+  expect(wrapper.find('tbody').children().length).toEqual(1);
+  expect(
+    wrapper
+      .find('tbody tr td')
+      .first()
+      .text()
+  ).toEqual('Account 2');
+
+  expect(
+    wrapper
+      .find('tbody tr td')
+      .at(5)
+      .text()
+  ).toEqual('2019-05-14');
 });
