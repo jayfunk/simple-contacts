b7019e544dd566ba1a46506112674ce5cc4ad136
Tyler Cook <tcook@ustudio.com>
2020-09-30 01:06:00 -0500

    Added a back button to the account component

--- src/Account.js
+++ src/Account.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 import AccountDetails from './AccountDetails';
 import Contacts from './Contacts';
@@ -7,6 +8,21 @@ import Contacts from './Contacts';
 function Account({account}) {
   return (
     <div className="container">
+      <Link to="/">
+        <svg
+          width="2em"
+          height="2em"
+          viewBox="0 0 16 16"
+          className="bi bi-arrow-left-circle-fill"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5.5a.5.5 0 0 0 0-1H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5z"
+          />
+        </svg>
+      </Link>
       <div className="row">
         <AccountDetails account={account} />
       </div>
