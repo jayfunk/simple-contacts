12206ace52544f9b657ca62b8675dd4beaa6ea27
Tyler Cook <tcook@ustudio.com>
2020-10-07 09:59:08 -0500

    Updates

---
+++ precache-manifest.cb6cd0b269e8f521d0804ac065391b91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e5dcf6cb05a1db13906f1dcc615ff6",
+    "url": "/simple-contacts/index.html"
+  },
+  {
+    "revision": "213cb9abc0f456ab76df",
+    "url": "/simple-contacts/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "62758a594cd1a95cde74",
+    "url": "/simple-contacts/static/css/main.91699584.chunk.css"
+  },
+  {
+    "revision": "213cb9abc0f456ab76df",
+    "url": "/simple-contacts/static/js/2.e4ad3fc3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/simple-contacts/static/js/2.e4ad3fc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62758a594cd1a95cde74",
+    "url": "/simple-contacts/static/js/main.35c17444.chunk.js"
+  },
+  {
+    "revision": "568ae4a3cba5f6babdf1",
+    "url": "/simple-contacts/static/js/runtime-main.e87ff679.js"
+  }
+]);
